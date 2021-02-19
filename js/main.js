1d13d0f4ffc07220b8ee7bfb59daddcff139cec5
diegoponciano85 <diegoponciano85@gmail.com>
2021-02-18 23:25:23 -0300

    js inicio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "Diego Ponciano";
+var n1 = 15;
+var n2 = 20;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + "Tem" + );
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase());
